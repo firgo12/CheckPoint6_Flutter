76a1a41905fc8a089d5b46682f1ec95cdb6a9375
Guilherme Mendes da Cunha <60897048+firgo12@users.noreply.github.com>
2024-11-19 20:10:25 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://fdxygukbptttglsxuapr.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkeHlndWticHR0dGdsc3h1YXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0MzQ2MzgsImV4cCI6MjA0NDAxMDYzOH0.U1FPYQzaUCp7DDkMaq-brtKDLB1tSRMo0Ym8zX0W1WY',
+    url: '',
+    anonKey: '',
   );
 
   runApp(MultiProvider(
